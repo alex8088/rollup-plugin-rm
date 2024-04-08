877e555593229b38ea96c97c283eec067cab0a5e
alex8088 <244096523@qq.com>
2024-04-08 22:07:17 +0800

    chore: simplify

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,10 @@ export default function rm(
   hook: 'buildStart' | 'buildEnd'
 ): Plugin | void {
   if (hook !== 'buildStart' && hook !== 'buildEnd') return
-  const _rm = async (target): Promise<void> => {
-    await fs.rm(target, { recursive: true, force: true }).catch(() => {})
-  }
   return {
     name: 'clean',
     [hook]: async (): Promise<void> => {
-      await _rm(target)
+      await fs.rm(target, { recursive: true, force: true }).catch(() => {})
     }
   }
 }
